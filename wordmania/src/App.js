a6486ff8441fb467c179a01a00da2151e021fcd3
helinaik11 <131780744+helinaik11@users.noreply.github.com>
2024-06-15 15:34:39 +0530

    Solved localhost-limited issues

--- wordmania/src/App.js
+++ wordmania/src/App.js
@@ -6,12 +6,39 @@ function App() {
   const [solution,setSolution]= useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/answers')
-    .then(res=>res.json())
-    .then(json => {
-      const randomSolution =json[Math.floor(Math.random()*json.length)]
+      const words=[{"id": 1, "word": "alter"},
+      {"id": 2, "word": "adieu"},
+      {"id": 3, "word": "dealt"},
+      {"id": 4, "word": "storm"},
+      {"id": 5, "word": "jiffy"},
+      {"id": 6, "word": "timer"},
+      {"id": 7, "word": "trust"},
+      {"id": 8, "word": "heart"},
+      {"id": 9, "word": "happy"},
+      {"id": 10, "word": "smile"},
+      {"id": 11, "word": "funny"},
+      {"id": 12, "word": "frost"},
+      {"id": 13, "word": "cards"},
+      {"id": 14, "word": "clamp"},
+      {"id": 15, "word": "hefty"},
+      {"id": 16, "word": "ninja"},
+      {"id":17, "word": "spade"},
+      {"id": 18, "word": "pools"},
+      {"id": 19, "word": "drive"},
+      {"id": 20, "word": "relax"},
+      {"id": 21, "word": "times"},
+      {"id": 22, "word": "train"},
+      {"id": 23, "word": "cores"},
+      {"id": 24, "word": "pours"},
+      {"id": 25, "word": "blame"},
+      {"id": 26, "word": "banks"},
+      {"id": 27, "word": "phone"},
+      {"id": 28, "word": "bling"},
+      {"id": 29, "word": "coins"},
+      {"id": 30, "word": "hello"}]
+      const randomSolution =words[Math.floor(Math.random()*words.length)]
       setSolution(randomSolution.word)
-    })
+    
   },[setSolution])
   return (
     <div className="App">
--- wordmania/src/components/keypad.js
+++ wordmania/src/components/keypad.js
@@ -4,11 +4,36 @@ export default function Keypad({usedKeys}){
     const [letters,setLetters]=useState(null)
 
     useEffect(()=>{
-        fetch('http://localhost:3001/letters')
-        .then(res=>res.json())
-        .then(json => {
-          setLetters(json)
-        }) 
+        const keys=[
+            {"key": "a"},
+            {"key": "b"},
+            {"key": "c"},
+            {"key": "d"},
+            {"key": "e"},
+            {"key": "f"},
+            {"key": "g"},
+            {"key": "h"},
+            {"key": "i"},
+            {"key": "j"},
+            {"key": "k"},
+            {"key": "l"},
+            {"key": "m"},
+            {"key": "n"},
+            {"key": "o"},
+            {"key": "p"},
+            {"key": "q"},
+            {"key": "r"},
+            {"key": "s"},
+            {"key": "t"},
+            {"key": "u"},
+            {"key": "v"},
+            {"key": "w"},
+            {"key": "x"},
+            {"key": "y"},
+            {"key": "z"}
+          ]
+          setLetters(keys)
+        
     },[])
     return (
         <div className="keypad">
